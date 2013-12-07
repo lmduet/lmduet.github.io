def48761ab19fc4e08d3fdeed9c50b5c8e543c83
Loïc Guychard <loic@bothrops.org>
2013-12-07 13:07:46 +0100

    Yeah

--- js/script.js
+++ js/script.js
@@ -1,26 +1,53 @@
 (function() {
     var rand = Math.random();
     if (rand < 0.3) {
-        document['pic'].src = 'img/3.jpg';
+        document['pic'].src = 'img/1.jpg';
     } else if (rand < 0.6) {
-        document['pic'].src = 'img/4.jpg';
+        document['pic'].src = 'img/2.jpg';
     };
-    document['pic'].style.display = "block";
+    document['pic'].classList.remove("hide");
 
     document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById("sect-toggle").onclick = function toggle() {
+        toggler = document.getElementById("sect-toggle");
+        toggler.onclick = function toggle() {
             var home = document.getElementById('home'),
-                concerts = document.getElementById('concerts');
+                concerts = document.getElementById('concerts'),
+                switcher = document.getElementById('switch');
 
-            if(home.style.display === "block") {
-                home.style.display = "none";
-                concerts.style.display = "block";
-            } else {
-                home.style.display = "block";
-                concerts.style.display = "none";
-            }
+            switcher.classList.add("hide");
+
+            setTimeout(function() {
+                if(toggler.innerHTML === "Concerts") {
+                    toggler.innerHTML = "Accueil";
+                    home.style.display = "none";
+                    concerts.style.display = "block";
+                } else {
+                    toggler.innerHTML = "Concerts";
+                    home.style.display = "block";
+                    concerts.style.display = "none";
+                }
+                switcher.classList.remove("hide");
+            }, 500);
+
         }
     })
+
+    function changeImage() {
+        var img = document.getElementById("pic");
+        img.classList.add("hide");
+        setTimeout(function(){
+            img.src = "img/" + x + ".jpg";
+            x++;
+            if(x >= 4){
+                x = 0;
+            }
+            img.classList.remove("hide");
+        }, 500);
+    }
+
+    var x = 0;
+
+    setInterval(changeImage, 6000);
 
 
 }).call(this);
