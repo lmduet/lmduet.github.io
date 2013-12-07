2a3a9898ad34016acf0b39966b4792eec818db62
Loïc Guychard <loic@bothrops.org>
2013-12-07 11:49:15 +0100

    New exports, start concerts thingy

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 (function() {
+
     var rand = Math.random();
     if (rand < 0.3) {
         document['pic'].src = 'img/2.jpg';
@@ -6,4 +7,21 @@
         document['pic'].src = 'img/3.jpg';
     };
     document['pic'].style.display = "block";
+
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("sect-toggle").onclick = function toggle() {
+            var home = document.getElementById('home'),
+                concerts = document.getElementById('concerts');
+
+            if(home.style.display === "block") {
+                home.style.display = "none";
+                concerts.style.display = "block";
+            } else {
+                home.style.display = "block";
+                concerts.style.display = "none";
+            }
+        }
+    })
+
+
 }).call(this);
